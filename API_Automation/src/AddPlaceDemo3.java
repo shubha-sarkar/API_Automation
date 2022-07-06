1b05b05e5eb6fb1b6b1c03c9ceefe42748221556
sarka <sarka@LAPTOP-7A521AL3>
2022-07-07 00:15:00 +0530

    Test

---
+++ API_Automation/src/AddPlaceDemo3.java
@@ -0,0 +1,21 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import files.Payload;
+public class AddPlaceDemo3 {
+
+	public static void main(String[] args) {
+
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		String response=given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body(Payload.AddPlace())
+		.when().post("maps/api/place/add/json")
+		.then().assertThat().statusCode(200).body("scope",equalTo("APP"))
+		.header("server","Apache/2.4.18 (Ubuntu)").extract().response().asString();
+		
+		System.out.println(response);
+		
+	}
+
+}
